9ba175f84700934993d41244271349bd5c700388
Gabriel <gaabrielserraa@gmail.com>
2023-03-14 20:46:28 -0400

    Cambios de Navbar y NavbarDrawer

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from "vue";
+import NavbarDrawer from "./NavbarDrawer.vue";
+
+const toggleLeftDrawer = ref(false);
+</script>
+
+<template>
+  <q-header elevated>
+    <q-toolbar>
+      <q-btn
+        flat
+        dense
+        round
+        icon="menu"
+        aria-label="Menu"
+        @click="toggleLeftDrawer = !toggleLeftDrawer"
+      />
+
+      <q-toolbar-title> Prueba Super-Heros </q-toolbar-title>
+
+    </q-toolbar>
+  </q-header>
+
+  <q-drawer
+    v-model="toggleLeftDrawer"
+    show-if-above
+    :width="270"
+    :breakpoint="400"
+  >
+    <NavbarDrawer />
+  </q-drawer>
+</template>
---
+++ src/components/NavbarDrawer.vue
@@ -0,0 +1,43 @@
+<script setup></script>
+
+<template>
+  <q-scroll-area
+    style="
+      height: calc(100% - 150px);
+      margin-top: 150px;
+      border-right: 1px solid #ddd;
+    "
+  >
+    <q-list padding>
+      <q-item to="/heros" clickable v-ripple>
+        <q-item-section avatar>
+          <q-icon name="inbox" />
+        </q-item-section>
+
+        <q-item-section> Consultas </q-item-section>
+      </q-item>
+
+      <q-item to="/heros/mod" clickable v-ripple>
+        <q-item-section avatar>
+          <q-icon name="star" />
+        </q-item-section>
+
+        <q-item-section> Modificaciones </q-item-section>
+      </q-item>
+    </q-list>
+  </q-scroll-area>
+
+  <q-img
+    class="absolute-top"
+    src="https://cdn.quasar.dev/img/material.png"
+    style="height: 150px"
+  >
+    <div class="absolute-bottom bg-transparent">
+      <q-avatar size="56px" class="q-mb-sm">
+        <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+      </q-avatar>
+      <div class="text-weight-bold">Gabriel Serra</div>
+      <div>@sg4bb</div>
+    </div>
+  </q-img>
+</template>
